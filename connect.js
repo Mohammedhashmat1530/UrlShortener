55886a12593fbcf7b11f973cf0f35ddc27c2ab10
hashmat1530 <hashmat2020vskp@gmail.com>
2023-12-09 12:51:44 +0530

    connect.js problem resolved

--- connect.js
+++ connect.js
@@ -3,10 +3,10 @@ const mongoose = require('mongoose');
 
 async function ConnectDB() {
   mongoose.connect('mongodb://localhost:27017/urlShortener', {
-    
+    family: 4,
   });
 }
 
 module.exports = {
   ConnectDB,
-};
+}
